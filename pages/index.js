958207d858503931c6a95f402e48122463dc20f4
Stephanie Willhide <stephaniewillhide@gmail.com>
2022-05-22 21:56:56 -0600

    Update CSS

--- pages/index.js
+++ pages/index.js
@@ -29,24 +29,26 @@ export default function Home() {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
       </Head>
 
-      <main className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-6 text-center">
-            <h1 className="pt-3 pt-md-5">Friendly Invite Generator</h1>
-            <img src="/wave.png" height="114" width="114" alt="hand waving icon" className="w-25 p-3" />
-            <p>Enter your friend's name, an idea for a plan, and the day, and we'll craft a cheerful message to send to them.</p>
-            <form onSubmit={onSubmit}>
-              <input
-                className="form-control"
-                type="text"
-                name="plan"
-                placeholder="Ex: Sam, coffee, Thursday"
-                value={planInput}
-                onChange={(e) => setPlanInput(e.target.value)}
-              />
-              <input type="submit" className="btn btn-info m-3" value="Generate plan" />
-            </form>
-            <div className="alert alert-primary">{result}</div>
+      <main className={styles.main}>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-6 text-center">
+              <h1 className="pt-3 pt-md-5">Friendly Invite Generator</h1>
+              <img src="/wave.png" height="114" width="114" alt="hand waving icon" className="w-25 p-3" />
+              <p>Enter your friend's name, an idea for a plan, and the day, and we'll craft a cheerful message to send to them.</p>
+              <form onSubmit={onSubmit}>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="plan"
+                  placeholder="Ex: Sam, coffee, Thursday"
+                  value={planInput}
+                  onChange={(e) => setPlanInput(e.target.value)}
+                />
+                <input type="submit" className="btn btn-info m-3" value="Generate plan" />
+              </form>
+              <div className="alert alert-primary">{result}</div>
+            </div>
           </div>
         </div>
       </main>
