025dc17286a8315959dee0cc98da1c9f49d9da65
Stephanie Willhide <stephaniewillhide@gmail.com>
2022-05-22 18:32:24 -0600

    Initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,22 +6,25 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-002", {
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+  const completion = await openai.createCompletion("text-curie-001", {
+    prompt: generatePrompt(req.body.plan),
+    temperature: 0.7,
+    max_tokens: 64,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(plan) {
+  return `Write an invitation to a friend based on these notes.
+  Prompt: Kurt, park, thursday
+  Invitation: Hi Kurt, I hope you are having a great day! Would you like to go to the park with me on Thursday?
+  Prompt: Jessica, coffee, saturday afternoon
+  Invitation: Hi there Jessica, how have you been? Would you like to get some coffee together on Saturday afternoon?
+  Prompt: Isabel, drinks, Monday after work
+  Invitation: Hey Isabel, hope all is well! I was wondering if you wanted to get drinks on Monday after work. Let me know if that sounds good to you!
+  Prompt: ${plan}
+  Invitation:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [planInput, setPlanInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -13,34 +13,42 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ plan: planInput }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    setPlanInput("");
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Cheerful Invite Generator</title>
+        <link rel="icon" href="/wave.png" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
       </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
+      <main className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6 text-center">
+            <h1 className="pt-3 pt-md-5">Friendly Invite Generator</h1>
+            <img src="/wave.png" height="114" width="114" alt="hand waving icon" className="w-25 p-3" />
+            <p>Enter your friend's name, an idea for a plan, and the day, and we'll craft a cheerful message to send to them.</p>
+            <form onSubmit={onSubmit}>
+              <input
+                className="form-control"
+                type="text"
+                name="plan"
+                placeholder="Ex: Sam, coffee, Thursday"
+                value={planInput}
+                onChange={(e) => setPlanInput(e.target.value)}
+              />
+              <input type="submit" className="btn btn-info m-3" value="Generate plan" />
+            </form>
+            <div className={styles.result}>{result}</div>
+          </div>
+        </div>
       </main>
     </div>
   );
